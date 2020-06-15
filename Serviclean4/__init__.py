cc6b8fe5d197c30687f241112b0bc78178573dbc
mmastermind <cienfuegos.gc@gmail.com>
2020-06-15 17:52:50 -0500

    dnspython installed

--- Serviclean4/__init__.py
+++ Serviclean4/__init__.py
@@ -8,7 +8,7 @@ from .extensions import mongo
 app = Flask(__name__)
 
 #imports from settings.py the mongo URI
-config_object='Serviclean_v2.settings'
+config_object='Serviclean4.settings'
 #configs the app with the previous settings and initializes it
 app.config.from_object(config_object)
 mongo.init_app(app)
