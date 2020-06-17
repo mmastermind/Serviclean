7cf82d24a9e41f0ff4e4e323f0bd69db745596fb
mmastermind <cienfuegos.gc@gmail.com>
2020-06-17 15:12:42 -0500

    Updated register to re-confirm password and minor spanish language fixes

--- Serviclean4/views.py
+++ Serviclean4/views.py
@@ -55,7 +55,6 @@ def login():
         #if bcrypt.hashpw(request.form['password'].encode(encoding='UTF-8'), login_user['password'].encode(encoding='UTF-8')) == login_user['password'].encode(encoding='UTF-8'):
         if request.form['password'] == login_user['password']:
             session['username'] = request.form['username']
-            #return redirect(url_for('home'))
             #Change of return by returning a template of home insted of redirect
             print('Llegue hasta aqui')
             return render_template(
@@ -74,9 +73,7 @@ def register_home():
     if request.method == 'POST':
         profiles = mongo.db.profiles
         existing_profile = profiles.find_one({'Perfil' : request.form['perfil']})
-        print(existing_profile)
         if existing_profile['Perfil'] == 'Colaborador':
-            print('llegue al colaborador')
             return render_template(
                 'register.html',
                 title='Registro pagina 2',
@@ -84,7 +81,6 @@ def register_home():
                 message='Por favor ingrese el resto de datos solicitados'
             )
         elif existing_profile['Perfil'] == 'Supervisor' or existing_profile['Perfil'] == 'Cliente':
-            print('llegue al supervisor / cliente')
             return render_template(
                 'register.html',
                 title='Registro Avanzado',
@@ -104,26 +100,36 @@ def register():
         users = mongo.db.users
         existing_user = users.find_one({'name' : request.form['username']})
 
-        if existing_user is None:
-            #change removing encode('utf-8') because of debugging issue: 'bytes' object has no attribute 'encode'
-            #hashpass = bcrypt.hashpw(request.form['pass'], bcrypt.gensalt())
-            #original line
-            #hashpass = bcrypt.hashpw(request.form['password'].encode(encoding='UTF-8'), bcrypt.gensalt())
-            #users.insert({'name' : request.form['username'], 'password' : hashpass})
-            users.insert({'name' : request.form['username'], 
-                          'password' : request.form['password'], 
-                          'email' : request.form['user_email'], 
-                          'keyword': request.form['keyword']})
-            #<input type="email" class="form-control" id="user_email" placeholder="nombre@ejemplo.com">
-            session['username'] = request.form['username']
-            #return redirect(url_for('login'))
-            #change for render template
+        if existing_user is None: 
+            if request.form['password'] == request.form['password_rep']:
+                #original line
+                #hashpass = bcrypt.hashpw(request.form['password'].encode(encoding='UTF-8'), bcrypt.gensalt())
+                #users.insert({'name' : request.form['username'], 'password' : hashpass})
+                users.insert({'name' : request.form['username'], 
+                              'password' : request.form['password'], 
+                              'email' : request.form['user_email'], 
+                              'keyword': request.form['keyword']})
+                #<input type="email" class="form-control" id="user_email" placeholder="nombre@ejemplo.com">
+                session['username'] = request.form['username']
+                #return redirect(url_for('login'))
+                #change for render template
+                return render_template(
+                    'home.html',
+                    title='Home',
+                    year=datetime.now().year,
+                    message='Has ingresado con exito' + session['username']
+                )
             return render_template(
-                'home.html',
-                title='Home',
+                'register.html',
+                title='Registro Avanzado',
                 year=datetime.now().year,
-                message='Has ingresado con exito' + session['username']
+                message='Las contraseñas no coinciden, favor de verificar!'
+                )
+        return render_template(
+            'register.html',
+            title='Registro Avanzado',
+            year=datetime.now().year,
+            message='Nombre de usario existente / ya registrado!'
             )
-        return 'Nombre de usario existente / ya registrado!'
 
     return render_template('register.html')
