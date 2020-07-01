49c979a4ee8f9f7a999cc4fb2e293ee1dbbb9d56
mmastermind <cienfuegos.gc@gmail.com>
2020-07-01 16:23:15 -0500

    Inital working flask_login connected with mongo, pending code cleaning

--- Serviclean4/views.py
+++ Serviclean4/views.py
@@ -4,36 +4,26 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template, url_for, request, session, redirect, flash
-from flask_login import login_required, current_user, UserMixin, login_user
+from flask_login import login_required, current_user, UserMixin, login_user, logout_user
 #imports from init.py the initialized app
 from Serviclean4 import app
 #imports required extensions
 from .extensions import mongo 
 from .extensions import login_manager
+from bson.objectid import ObjectId
 
-#users = mongo.db.users
-users = {'foo@bar.tld': {'password': 'secret'}}
+users = mongo.db.users
+#users = {'foo@bar.tld': {'password': 'secret'}}
+user_login = None
 
-print(users)
+#print(users)
 class User(UserMixin):
     pass
 
-@login_manager.user_loader
-def user_loader(user_login):
-#    users = mongo.db.users    
-    print('here')
-    if user_login not in users:
-        return ('Pelation time')
-
-    user = User()
-    user.id = user_login
-    print('se uso user_loader')
-    return user
-
-
 @app.route('/logout')
+@login_required
 def logout():
-    flask_login.logout_user()
+    logout_user()
     return 'Logged out'
 
 @login_manager.unauthorized_handler
@@ -74,47 +64,68 @@ def about():
         message='Your application description page.'
     )
 
+@login_manager.user_loader
+def user_loader(user_login):
+    
+    print('inicio user_loader ' + user_login)
+    #user_login = users.find_one({'name' : request.form['username']})
+    user_login_temp = users.find_one({'name' : user_login})
+    print(user_login_temp)
+    #if user_login_temp not in users:
+    if user_login_temp:
+        user = User()
+        user.id = user_login
+        print('fin uso user_loader ' + user.id)
+        
+        return user
+    return None
+
 @login_manager.request_loader
 def request_loader(request):
-#    users = mongo.db.users
+
+    print('inicio_request_loader')
     username = request.form.get('username')
-    if username not in users:
-        return ('cucumber')
-
-    user = User()
-    user.id = username
-    user.is_authenticated = request.form['password'] == users[user_login]['password']
-    print('aqui')
-
-    return user
+    #Linea agregada para evitar "collection not iterable":
+    user_login_temp = users.find_one({'name' : ObjectId(username)})
+    print(user_login_temp)
+    if user_login_temp:
+        user = User()
+        user.id = username
+        user.is_authenticated = request.form['password'] == users[user_login]['password']
+        print('fin_request_loader' + user.id)
+        
+        return (user)
+    return ('No ingreso ', None)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    #aqui borre flask.request.method
+ 
+    #user_login = None
     if request.method == 'GET':
         return (login.html)
 
     #Defines the collection of users
     #users = mongo.db.users
-    #user_login = users.find_one({'name' : request.form['username']})
+ 
     
-    #aqui borre flask.request.form
     user_login = request.form['username']
+    print('login route: ' + user_login)
     if user_login:
+        db_login = users.find_one({'name' : user_login})
         #change removing encode('utf-8') because of debugging issue: 'bytes' object has no attribute 'encode'
         #original line:
         #if bcrypt.hashpw(request.form['password'].encode(encoding='UTF-8'), login_user['password'].encode(encoding='UTF-8')) == login_user['password'].encode(encoding='UTF-8'):
-        
+        print('login: ')
+        print(db_login)
        # debugging purposes    if request.form['password'] == user_login['password']:
-        if request.form['password'] == users[user_login]['password']:
+        if request.form['password'] == db_login['password']:
             session['username'] = request.form['username']
-
              # Login and validate the user.
             user = User()
             user.id = user_login
             login_user(user)
-
-            print('se ha ingresado a: ')
+            print(user.id)
+            print('se ha ingresado a: ' + user.id)
 
             flash('Ingreso exitoso.', category = 'message')  
 
@@ -156,6 +167,7 @@ def login():
 @app.route('/register_home', methods=['POST', 'GET'])
 def register_home():
     profiles = mongo.db.profiles
+#    Code line to input directly a new profile in the database:
 #    profiles.insert({'Perfil' : 'Supervisor'})
     if request.method == 'POST':
         profiles = mongo.db.profiles
